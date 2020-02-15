7997320f21cfa9672135a56284e4ebd9c48ef97e
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-15 22:56:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,56 @@ class Song
     @@all
   end
 
+  def self.create
+      song = self.new
+      song.all << song
+      song
+  end
+  
   def save
     self.class.all << self
+  end
+  
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name 
+    @@all << self
+    song
+    
+  end
+  
+  def self.find_by_name(name)
+    @@all.find{|song| song.name == name}
+  end
+  
+  def self.find_or_create_by_name
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+  
+  def self.alphabetical
+    self.all.sort_by! {|song| song.name == name}
+  end
+
+  def self.new_from_filename
+    
+  end
+  
+  def self.create_from_filename
+    
+  end
+  
+  def self.destroy_all
+    self.all.clear
   end
 
 end
