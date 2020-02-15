23e51c551bb8155d0987a09aa3db57dac7edf809
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-15 23:21:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,17 +47,7 @@ class Song
     song
   end
   
-   #
-  # def self.new_from_filename(filename)
-  #   title = filename.split(" - ")
-  #   artist = title[0]
-  #   song_name, extension = title[1].split(".")
 
-  #   song = self.new 
-  #   song.artist_name = artist 
-  #   song.name = song_name 
-  #   song 
-  # end
   
   def self.create_from_filename
     
